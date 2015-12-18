d4dce45833405f207da39bfaa4c962bd4c51fc1e
RoxtonLabs <roxtonlabs@gmail.com>
2015-12-18 16:10:32 +0000

    Create first draft

---
+++ Doc-to-RTF-One-Click/Doc-to-RTF-One-Click/ConversionForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Doc_to_RTF_One_Click
+{
+    partial class ConversionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 13);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(278, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(13, 43);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(54, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Loading...";
+            // 
+            // ConversionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 73);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Name = "ConversionForm";
+            this.Text = "Doc to RTF Conversion";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.ConversionForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ Doc-to-RTF-One-Click/Doc-to-RTF-One-Click/ConversionForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;    //For the directory handling
+using Word = Microsoft.Office.Interop.Word; //Right-click solution, Add>Rference>Microsoft Word 14.0 Object Library
+
+namespace Doc_to_RTF_One_Click
+{
+    public partial class ConversionForm : Form
+    {
+        public ConversionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void convertFiles()
+        {   //Converts all the .doc and .docxf files in the folder into .rtf
+            //UI faffery
+
+            //First, create the output directory if it doesn't exist
+            string curDir = Application.StartupPath;
+            if (!Directory.Exists(curDir + @"\Output"))
+            {
+                statusLabel.Text = "Creating output directory...";
+                Directory.CreateDirectory(curDir + @"\Output");
+            }
+
+            //Next, get a list of all the eligible files in the current folder
+            statusLabel.Text = "Collating files...";
+            string[] fileNames = Directory.GetFiles(curDir, "*.doc*");
+            List<string> converted = new List<string>();    //This list will hold all the files we have successfully converted
+
+            //UI faffery
+            progressBar.Minimum = 0;
+            progressBar.Maximum = fileNames.Length;
+            progressBar.Value = 0;
+            progressBar.Step = 1;
+
+            //Create our Word objects
+            try
+            {
+                statusLabel.Text = "Initialising MS Word...";
+                Word.Application wordApp = new Word.Application();
+                wordApp.Visible = true; //For development purposes at least; turn this off for production
+                Word.Document wordDoc;
+                object unknown = Type.Missing;  //Object needed for the save method
+                object format = Word.WdSaveFormat.wdFormatRTF;
+                //Start Word and get transforming
+                foreach (string fileName in fileNames)
+                {   //Open the file
+                    progressBar.PerformStep();
+                    object newName = curDir + @"\Output\" + Path.GetFileNameWithoutExtension(fileName) + ".rtf";
+                    try
+                    {   //Often an error is returned even if the file works perfectly
+                        statusLabel.Text = "Opening " + Path.GetFileName(fileName).ToString() + "...";
+                        wordDoc = wordApp.Documents.Open(fileName);
+                        wordDoc.SaveAs2(newName, format);
+                        wordDoc.Close();
+                        converted.Add(fileName);    //Add the converted file to the list
+                    }
+                    catch (Exception) { }
+                }
+
+                //Close Word and release our objects
+                //Not entirely sure if releasing them is necessary but better safe etc.
+                statusLabel.Text = "Closing MS Word...";
+                wordApp.Quit();
+                releaseObject(wordApp);
+                wordDoc = null; //We have to do this because of that damn try-loop
+                releaseObject(wordDoc);
+
+                //Delete the files we've converted (if we can)
+                statusLabel.Text = "Deleting input files...";
+                string[] convertedFiles = converted.ToArray();
+                foreach(string fileName in convertedFiles)
+                {
+                    try
+                    {
+                        File.Delete(fileName);
+                    }
+                    catch (Exception) { }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not open Microsoft Word.");
+                Environment.Exit(0);
+            }
+        }
+
+        //Release interop objects
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception x)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();   //Call in the binman
+            }
+        }
+
+        private void ConversionForm_Shown(object sender, EventArgs e)
+        {
+            convertFiles();
+            Environment.Exit(1);
+        }
+    }
+}
